24c1f4f7fe10241dc4cde1cebfebce778d75e418
Eduardo Gonzalez Paniagua <eduardo.gonzalez@razorvision.net>
2024-08-31 21:40:50 -0600

    upload files

--- src/app/Shared/popups/files-upload/files-upload.module.ts
+++ src/app/Shared/popups/files-upload/files-upload.module.ts
@@ -3,19 +3,24 @@ import { CommonModule } from '@angular/common';
 import { FilesUploadComponent } from './files-upload.component';
 import { FilesUploadDirective } from './files-upload.directive';
 import { MatDialogModule } from '@angular/material/dialog';
+import { DropZoneDirective } from './directives';
+import { UploadComponent } from './components/upload/upload.component';
 
 
 @NgModule({
   declarations: [
     FilesUploadComponent,
-    FilesUploadDirective
+    FilesUploadDirective,
+    DropZoneDirective,
+    UploadComponent
   ],
   imports: [
     CommonModule,
     MatDialogModule
   ],
   exports: [
-    FilesUploadDirective
+    FilesUploadDirective,
+    DropZoneDirective
   ]
 })
 export class FilesUploadModule { }
