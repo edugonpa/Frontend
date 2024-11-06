4373321466c779dffa1a3eac4b2b9435e6602f79
Eduardo Gonzalez Paniagua <eduardo.gonzalez@razorvision.net>
2024-11-05 19:49:13 -0600

    FE changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,10 +22,19 @@ import { PopupsModule } from './shared/popups';
 
 import { NotificationModule } from './services';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './components/header/header.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MenuListComponent } from './components/menu-list/menu-list.component';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuListComponent
   ],
   imports: [
     BrowserModule,
@@ -45,7 +54,12 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     BrowserAnimationsModule,
     PopupsModule,
     NotificationModule.forRoot(),
-    MatSidenavModule
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/auth/pages/login/login.component.ts
@@ -0,0 +1,21 @@
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  loginUsuario(form: NgForm){
+    
+  }
+
+}
