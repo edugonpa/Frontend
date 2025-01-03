53c13e165f90cd835f309fe4b0469dc583085e1e
Eduardo Gonzalez Paniagua <eduardo.gonzalez@razorvision.net>
2025-01-02 22:58:50 -0600

    pipes en authorization

--- src/app/store/user/user.effects.ts
+++ src/app/store/user/user.effects.ts
@@ -4,7 +4,8 @@ import { HttpClient } from '@angular/common/http';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { NotificationService } from '@app/services';
 import { Router } from '@angular/router';
-import { catchError, map, Observable, of, switchMap, tap } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, map, switchMap, tap } from 'rxjs/operators';
 import { UserResponse } from './user.models';
 import { environment } from 'environments/environment';
 
@@ -36,6 +37,53 @@ export class UserEffects {
             return of(new fromActions.SignUpEmailError(err.message));
           })
         )
+      )
+    )
+  );
+
+  signInEmail: Observable<Action> = createEffect(() =>
+    this.actions.pipe(
+      ofType(fromActions.Types.SIGN_IN_EMAIL),
+      map((action: fromActions.SignUpEmail) => action.user),
+      switchMap( userData =>
+        this.httpClient.post<UserResponse>(`${environment.url}account/login-app/`, userData)
+        .pipe(
+          tap((response: UserResponse) => {
+            localStorage.setItem('token', response.token.access);
+            this.router.navigate(['/']);
+          }),
+          map((response: UserResponse) => new fromActions.SignInEmailSuccess(response.email, response || null)),
+          catchError(err => {
+            this.notification.error("Las credenciales son incorrectas");
+            return of(new fromActions.SignInEmailError(err.message));
+          })
+        )
+      )
+    )
+  );
+
+  init: Observable<Action> = createEffect(() =>
+    this.actions.pipe(
+      ofType(fromActions.Types.INIT),
+      switchMap( async() => localStorage.getItem('token')),
+      switchMap( token =>
+      {
+        if(token){
+          return this.httpClient.get<UserResponse>(`${environment.url}account/session/`)
+          .pipe(
+            tap((response: UserResponse) => {
+              console.log('data del usuario en sesion que viene del servidor', response);
+            }),
+            map((response: UserResponse) => new fromActions.InitAuthorized(response.email, response || null)),
+            catchError(err => {
+              return of(new fromActions.InitError(err.message));
+            })
+        )
+        }else{
+          return of(new fromActions.InitUnAuthorized());
+        }
+      }
+
       )
     )
   );
